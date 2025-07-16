32df00f42c6deef9839410aee64f7e64a1c2aed7
user <user@DESKTOP-0FTCJBS>
2025-07-16 17:03:07 +0300

    Video2,3

--- DEMO5/Program.cs
+++ DEMO5/Program.cs
@@ -34,9 +34,9 @@
             ////Animal=Dog => Dog is Animal 
 
 
-            object O1 =3;
+            //object O1 =3;
 
-            int X =(int)O1;
+            //int X =(int)O1;
 
             //Explicit Casting : 
             //Unsafe  Casting
@@ -45,6 +45,126 @@
             //Child = Parent => Parent is Child
 
             #endregion
+
+            #region NullableType
+            //NullableType
+
+            //Value Type
+            //Reference Type 
+
+            //int X = 6;
+
+            //X=null; NotValid
+
+
+            //Nullable Value type =>Value Type Allow Null as valid Value
+
+            //Nullable<int> :Allow Int value Or Null as Null as Valid Value
+            //int? Age = 20;
+
+            //Age = null;
+
+
+
+            ////Nullable <Double> : Allow double Value Or Null As Valid Value
+            //double? Salary=4000.5;
+
+            //Salary = null;
+
+
+
+            //int X = 5;
+
+            ////Y:Nullable<int>
+            ////Y:Allow Int Value Or Null
+
+            //int? Y = X;
+
+            ////Implicit Casting
+            ////Safe Casting 
+
+
+            //Console.WriteLine(Y);
+
+
+
+            //int? X = 5;
+
+            //X = null;
+            //X = null;
+
+            //Y : int 
+            //Y :Allow Int Value Only
+
+            //int Y ; //System.InvalidOperationException: Nullable object must have a value.
+
+            //Explicit Castign
+            //May Throw EXception
+
+
+            //Defensive Code
+
+            //if (X != null)
+            //    Y = (int)X;
+            //else
+            //    Y = 0;
+
+
+            //=====================
+
+            //if (X is not  null)
+            //    Y = (int)X;
+            //else
+            //    Y = 0;
+
+
+            //=====================
+
+
+            //if (X.HasValue)
+            //    Y = X.Value;
+            //else
+            //    Y = 0;
+
+            //======================
+
+            //Y = X.HasValue ? X.Value : 0;
+
+
+            //========================
+
+
+            //Y = X is not null ? X.Value : 0;
+
+
+
+            //========================
+
+            //Null Coalescing Operator ??
+
+            //Y = X ?? 0;  //Syntax Sugar :X is not null ? X.value :0
+
+            //Console.WriteLine(Y); 
+            #endregion
+
+            #region nullable reference type
+            //nullable reference type
+
+            //string Message01= null; //Required
+
+
+            //String? Message02 = null; //Nullable
+
+            //Console.WriteLine(Message01.Length);
+            //Console.WriteLine(Message02);
+
+            #endregion
+
+
+
+
+
+
 
         }
     }
