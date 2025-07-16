aa84a37747663efef4ec065c3a06a2efd982ae96
user <user@DESKTOP-0FTCJBS>
2025-07-16 15:07:35 +0300

    Add project files.

---
+++ DEMO5/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DEMO5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Video 1
+
+            #endregion
+
+        }
+    }
+}
