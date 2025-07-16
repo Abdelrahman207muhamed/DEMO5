9af8566a1e1a6d33ea60c2545d5a3ea3040a3c02
user <user@DESKTOP-0FTCJBS>
2025-07-16 15:36:24 +0300

    Video 1

--- DEMO5/Program.cs
+++ DEMO5/Program.cs
@@ -2,9 +2,47 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            #region Video 1
+            #region  Boxing And Un Boxing
+            //Boxing And Un Boxing
+
+            //Boxing   : Casting From ValueType To ReferenceType
+            //Unboxing :Casting From ReferenceType To ValueType
+
+            //Object O1;
+            //O1 => Can Refer to instant Of "Object" Or Any Instant  of any Type    
+
+            //O1=1;
+            //O1=1.5;
+            //O1="Ahmed"
+
+
+            //O1 = 3; //Cast from ValueType [int] To RefernceType [object] =>Boxing
+            //O1 = 1.5;//Cast From ValueType [double] to ReferenceType[object]=>Boxing
+            //O1 = 'A';//Cast From ValueType [char] to ReferenceType[object]=>Boxing
+            //O1 = true;//Cast From ValueType [bool] to ReferenceType[object]=>Boxing
+            //O1 = new DateTime();//Cast From ValueType [Datetime] to ReferenceType[object]=>Boxing
+
+            //int X = 5;
+            //object O1 = /*(object)*/X;
+
+            ////inmplicit Casting 
+            ////Safe Casting
+            /////Boxing
+            ////Parent => Child :Child is Parent
+            ////Animal=Dog => Dog is Animal 
+
+
+            object O1 =3;
+
+            int X =(int)O1;
+
+            //Explicit Casting : 
+            //Unsafe  Casting
+            //UnBoxing Cast from ReferenceType [int] To ValueType [object] => UnBoxing
+            //Dog = (Dog) Animal =>Animal  is Dog
+            //Child = Parent => Parent is Child
 
             #endregion
 
